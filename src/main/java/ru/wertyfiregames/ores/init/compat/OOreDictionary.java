8abc71ad837210366db0d36fa2f896fcd0d7d174
Wertyfire <sw405@yandex.ru>
2024-07-16 19:43:49 +0300

    Added silver and tin ores to ore dictionary

--- src/main/java/ru/wertyfiregames/ores/init/compat/OOreDictionary.java
+++ src/main/java/ru/wertyfiregames/ores/init/compat/OOreDictionary.java
@@ -11,7 +11,15 @@ import ru.wertyfiregames.ores.init.OItems;
 public class OOreDictionary {
     public static void register() {
         OreDictionary.registerOre("oreCopper", OBlocks.copper_ore);
-        OreDictionary.registerOre("ingotCopper", OItems.copper_ingot);
         OreDictionary.registerOre("blockCopper", OBlocks.copper_block);
+        OreDictionary.registerOre("ingotCopper", OItems.copper_ingot);
+
+        OreDictionary.registerOre("oreSilver", OBlocks.silver_ore);
+        OreDictionary.registerOre("blockSilver", OBlocks.silver_block);
+        OreDictionary.registerOre("ingotSilver", OItems.silver_ingot);
+
+        OreDictionary.registerOre("oreTin", OBlocks.tin_ore);
+        OreDictionary.registerOre("blockTin", OBlocks.tin_block);
+        OreDictionary.registerOre("ingotTin", OItems.tin_ingot);
     }
 }
